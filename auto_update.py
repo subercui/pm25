d87a4f437fcdbb968be3dc976b3c0f4801c56f2e
suber <subercui@sina.com>
2015-06-12 10:39:11 +0800

    add files

---
+++ auto_update.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#几个bash命令，以及调用的.py文件内的bash命令，访问顺序是什么样的
+#用于每日调用，更新model以及依赖文件
+
+import sys
+import os
+#prepare data
+#gfs
+os.system('python ./datacode/download_gfs.py')
+gfsdir='/ldata/pm25data/gfs/'
+os.system('find '+gfsdir+' -type f -size 0 -exec rm -f {} \;')
+#pm25 mean
+os.system('python ./datacode/pm25_mean.py')
+
+#update dataset
+os.system('python ./datacode/pm25_dataset_maker0518.py')
+os.system('python ./datacode/pm25_dataset_maker0605.py')
+
+#update model
+os.system('python ./model/pm25_mlp.py')
+os.system('python ./model/pm25_mlp0605.py')
