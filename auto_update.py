6cf2ffe570b31c480334e0026b86da01ba2cd23d
suber <subercui@sina.com>
2015-06-12 16:06:18 +0800

    add auto

--- auto_update.py
+++ auto_update.py
@@ -9,15 +9,18 @@ import os
 os.system('python ./datacode/download_gfs.py')
 print "gfs downloading finished"
 gfsdir='/ldata/pm25data/gfs/'
-print "empty gfs files deleted"
 os.system('find '+gfsdir+' -type f -size 0 -exec rm -f {} \;')
+print "empty gfs files deleted"
 #pm25 mean
 os.system('python ./datacode/pm25_mean.py')
+print "pm25 mean generated"
 
 #update dataset
-os.system('python ./datacode/pm25_dataset_maker0518.py')
+#os.system('python ./datacode/pm25_dataset_maker0518.py')
 os.system('python ./datacode/pm25_dataset_maker0605.py')
+print "dataset generated"
 
 #update model
-os.system('python ./model/pm25_mlp.py')
+#os.system('python ./model/pm25_mlp.py')
 os.system('python ./model/pm25_mlp0605.py')
+print "model trained"
