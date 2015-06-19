a5bfd2c4c0c81da187e244e6971cd3d82ef49f76
suber <subercui@sina.com>
2015-06-19 09:19:36 +0800

    fix

--- auto_update.py
+++ auto_update.py
@@ -5,29 +5,32 @@
 import sys
 import os
 from datetime import datetime
+
+today=datetime.today()
+print "begin at "+today.strftime('%Y-%m-%d %H:%M')
+
 #prepare data
 #gfs
-os.system('python ./datacode/download_gfs.py')
+os.system('python /home/suber/projects/pm25/datacode/download_gfs.py')
 print "gfs downloading finished"
 gfsdir='/ldata/pm25data/gfs/'
 os.system('find '+gfsdir+' -type f -size 0 -exec rm -f {} \;')
 print "empty gfs files deleted"
 #pm25 mean
-os.system('python ./datacode/pm25_mean.py')
+os.system('python /home/suber/projects/pm25/datacode/pm25_mean.py')
 print "pm25 mean generated"
 
 #update dataset
-#os.system('python ./datacode/pm25_dataset_maker0518.py')
-os.system('python ./datacode/pm25_dataset_maker0605.py')
+#os.system('python /home/suber/projects/pm25/datacode/pm25_dataset_maker0518.py')
+os.system('python /home/suber/projects/pm25/datacode/pm25_dataset_maker0605.py')
 print "dataset generated"
 
 #update model
-#os.system('python ./model/pm25_mlp.py')
-os.system('python ./model/pm25_mlp0605.py')
+#os.system('python /home/suber/projects/pm25/model/pm25_mlp.py')
+os.system('python /home/suber/projects/pm25/model/pm25_mlp0605.py')
 print "model trained"
 
 #rsync
-today=datetime.today()
 os.system('rsync -av /ldata/pm25data/pm25model/MlpModel'+today.strftime('%Y%m%d')+'.pkl.gz caiyun@10.144.246.254:/ldata/pm25data/pm25model/')
 os.system('rsync -avr /ldata/pm25data/pm25mean/mean'+today.strftime('%Y%m%d')+' caiyun@10.144.246.254:/ldata/pm25data/pm25mean/')
 print "rsync finished"
